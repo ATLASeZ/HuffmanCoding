8c4559c93ef08d8fa6949f2188af8e16d1fb4f13
ATLASeZ <113227667+ATLASeZ@users.noreply.github.com>
2023-05-22 11:46:20 +0000

    Starting develop code, create class huffmantree

--- main.cpp
+++ main.cpp
@@ -1,5 +1,82 @@
 #include <iostream>
-
-int main() {
-  std::cout << "Hello World!\n";
-}
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <string>
+ 
+/* Структура Node, представляющая узел в дереве Хаффмана */
+struct Node
+{
+    char m_char;               // Символ, представленный этим узлом
+    int m_frequency;           // Частота этого символа в исходном тексте
+    Node* m_left;              // Указатель на левого потомка этого узла
+    Node* m_right;             // Указатель на правого потомка этого узла
+ 
+    /* Конструктор для создания нового узла */
+    Node(char m_char, int m_frequency, Node* m_left = nullptr, Node* m_right = nullptr)
+        : m_char(m_char), m_frequency(m_frequency), m_left(m_left), m_right(m_right) {}
+};
+ 
+/* Класс HuffmanTree */
+class HuffmanTree
+{
+private:
+    Node* root;                 // Корень дерева Хаффмана
+ 
+    /*
+    Структура NodeComparator используется для сравнения двух узлов по их частотам.
+    Это нужно для построения очереди с приоритетами, где узлы с меньшей частотой имеют более высокий приоритет.
+    */
+    struct NodeComparator
+    {
+        bool operator()(Node* left, Node* right)
+        {
+            return left->m_frequency > right->m_frequency;
+        }
+    };
+ 
+    void BuildTree(const std::unordered_map<char, int>& frequencyMap)
+    {
+        std::priority_queue<Node*, std::vector<Node*>, NodeComparator> minHeap;
+ 
+        for (auto pair : frequencyMap)
+        {
+            minHeap.push(new Node(pair.first, pair.second));
+        }
+ 
+        while (minHeap.size() != 1)
+        {
+            Node* m_left = minHeap.top(); minHeap.pop();
+            Node* m_right = minHeap.top(); minHeap.pop();
+ 
+            int sum = m_left->m_frequency + m_right->m_frequency;
+            minHeap.push(new Node('\0', sum, m_left, m_right));
+        }
+ 
+        root = minHeap.top();
+    }
+ 
+    /* Функция encodeHelper, рекурсивно обходящая дерево Хаффмана и строящая коды Хаффмана для каждого символа */
+    void EncodeHelper(Node* node, std::string str, std::unordered_map<char, std::string>& huffmanCode)
+    {
+        /* Если узел пуст, просто возвращаемся */
+        if (node == nullptr)
+        {
+            return;
+        }
+ 
+        /* Если узел является листом (то есть у него нет дочерних узлов),
+        то мы достигли символа и добавляем текущую строку в качестве его кода Хаффмана
+        */
+        if (!node->m_left && !node->m_right)
+        {
+            huffmanCode[node->m_char] = str;
+        }
+ 
+        /* Рекурсивно обходим левое поддерево, добавляя '0' к текущей строке */
+        EncodeHelper(node->m_left, str + "0", huffmanCode);
+        /* Рекурсивно обходим правое поддерево, добавляя '1' к текущей строке */
+        EncodeHelper(node->m_right, str + "1", huffmanCode);
+    }
+ 
