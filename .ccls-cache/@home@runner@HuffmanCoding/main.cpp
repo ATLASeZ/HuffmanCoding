f69a281f14238cae9b1519e76559fc49be9f4432
ATLASeZ <113227667+ATLASeZ@users.noreply.github.com>
2023-05-25 09:02:18 +0000

    Done lab, also develop creating 2 files with cipher and output

--- .ccls-cache/@home@runner@HuffmanCoding/main.cpp
+++ .ccls-cache/@home@runner@HuffmanCoding/main.cpp
@@ -79,4 +79,128 @@ private:
         /* Рекурсивно обходим правое поддерево, добавляя '1' к текущей строке */
         EncodeHelper(node->m_right, str + "1", huffmanCode);
     }
+
+public:
+    /*
+    Конструктор класса HuffmanTree, который принимает map символов и их частот
+    При создании объекта HuffmanTree, дерево Хаффмана строится автоматически
+    */
+    HuffmanTree(const std::unordered_map<char, int>& frequencyMap)
+    {
+        BuildTree(frequencyMap);
+    }
  
+    /*
+    Функция calculateFrequencies считывает файл и вычисляет частоту каждого символа в файле.
+    Возвращает map, где ключ - это символ, а значение - его частота
+    */
+    static std::unordered_map<char, int> calculateFrequencies(std::ifstream& inputFile)
+    {
+        std::unordered_map<char, int> frequencyMap;
+        char m_char;
+ 
+        while (inputFile.get(m_char))
+        {
+            frequencyMap[m_char]++;
+        }
+ 
+        return frequencyMap;
+    }
+ 
+    /*
+    Функция encode кодирует исходный файл с использованием дерева Хаффмана и записывает результат в выходной файл.
+    Возвращает коэффициент сжатия
+    */
+    double Encode(std::ifstream& inputFile, std::ofstream& outputFile)
+    {
+        inputFile.clear();                                                          // Сбросить состояние файла
+        inputFile.seekg(0);                                                         // Переместить указатель чтения в начало файла
+        std::unordered_map<char, int> frequencyMap = calculateFrequencies(inputFile);
+        BuildTree(frequencyMap);
+ 
+        inputFile.clear();
+        inputFile.seekg(0);
+        std::string str((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+ 
+        std::unordered_map<char, std::string> huffmanCode;
+        EncodeHelper(root, "", huffmanCode);
+ 
+        std::string encodedStr;
+        for (char m_char : str)
+        {
+            encodedStr += huffmanCode[m_char];
+        }
+ 
+        outputFile << encodedStr;
+ 
+        double compressionRatio = (double)encodedStr.size() / (str.size() * 8);
+ 
+        return compressionRatio;
+    }
+ 
+ 
+    bool Decode(std::ifstream& inputFile, std::ofstream& outputFile)
+    {
+        /* Считываем закодированную строку из файла */
+        std::string encodedStr((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+ 
+        Node* node = root;
+        std::string decodedStr;
+ 
+        /* Декодируем строку */
+        for (char bit : encodedStr)
+        {
+            if (bit == '0')
+            {
+                node = node->m_left;
+            }
+            else if (bit == '1')
+            {
+                node = node->m_right;
+            }
+ 
+            /* Если достигли листа, добавляем символ в декодированную строку и возвращаемся к корню */
+            if (node->m_left == nullptr && node->m_right == nullptr)
+            {
+                decodedStr += node->m_char;
+                node = root;
+            }
+        }
+ 
+        /* Записываем декодированную строку в выходной файл */
+        outputFile << decodedStr;
+ 
+        return true;
+    }
+};
+ 
+ 
+void WriteEncodedFile(std::ofstream& outputFile, const std::string& encodedStr)
+{
+    outputFile << encodedStr;
+}
+ 
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::ifstream inputFile("input.txt");
+    std::unordered_map<char, int> freqMap = HuffmanTree::calculateFrequencies(inputFile);
+    HuffmanTree huffmanTree(freqMap);
+ 
+    std::ofstream encodedFile("encoded.txt");
+    double compressionRatio = huffmanTree.Encode(inputFile, encodedFile);
+    std::cout << "Степень сжатия: " << compressionRatio << std::endl;
+ 
+    inputFile.close();
+    encodedFile.close();
+ 
+    std::ifstream encodedInputFile("encoded.txt");
+    std::ofstream decodedFile("decoded.txt");
+    bool isDecoded = huffmanTree.Decode(encodedInputFile, decodedFile);
+    std::cout << "Декодированние прошло " << (isDecoded ? "успешно" : "неудачно") << std::endl;
+ 
+    encodedInputFile.close();
+    decodedFile.close();
+ 
+    return 0;
+}
